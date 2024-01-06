1c80e048636b5ffe02d2d5a923a451b3b4f92564
VladYoSlav <vaifaer@gmail.com>
2024-01-06 03:14:09 +0300

    feat: overlay

--- src/drawer/ui/drawer.tsx
+++ src/drawer/ui/drawer.tsx
@@ -1,9 +1,11 @@
 import { Content } from './content'
+import { Overlay } from './overlay'
 import { Portal } from './portal'
 import { Root } from './root'
 
 export const Drawer = {
   Root,
   Content,
-  Portal
+  Portal,
+  Overlay
 }
