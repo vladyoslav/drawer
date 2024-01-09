c56e47c29810a0d9c2a213624dd5f2811c0b80c2
VladYoSlav <vaifaer@gmail.com>
2024-01-09 18:56:11 +0300

    chore: remove framer-motion from peer deps

--- src/drawer/lib/types.ts
+++ src/drawer/lib/types.ts
@@ -1,7 +1,3 @@
-import { type MotionProps } from 'framer-motion'
-
-export type WithoutMotionProps<T> = Omit<T, keyof MotionProps>
-
 export type OnOpenChange = (open: boolean) => void
 
 export type Snap = number | string
