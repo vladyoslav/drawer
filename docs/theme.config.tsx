916aab99e2b00d2ef9d2a25c5f897545a8c64a4c
VladYoSlav <vaifaer@gmail.com>
2024-01-31 23:25:23 +0300

    feat(docs): nextra

---
+++ docs/theme.config.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import { type DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>@vladyoslav/drawer</span>,
+  project: {
+    link: 'https://github.com/vladyoslav/drawer'
+  },
+  docsRepositoryBase: 'https://github.com/vladyoslav/drawer/docs',
+  footer: {
+    text: '@vladyoslav/drawer'
+  }
+}
+
+export default config
