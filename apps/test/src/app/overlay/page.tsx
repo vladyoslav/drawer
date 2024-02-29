42186aa13e089b0845f3e04d67bf50a5a2a1acd1
VladYoSlav <vaifaer@gmail.com>
2024-02-29 17:53:58 +0300

    feat(structure): move to apps and packages, rewrite configs

---
+++ apps/test/src/app/overlay/page.tsx
@@ -0,0 +1,47 @@
+'use client'
+
+import { Drawer } from '@vladyoslav/drawer'
+
+import { Content, Description, Header, Overlay, Trigger } from '@/shared/ui'
+
+const Page = () => {
+  return (
+    <>
+      <Drawer.Root>
+        <Trigger>Open drawer</Trigger>
+        <Drawer.Portal>
+          <Overlay />
+          <Content>
+            <Header>Drawer header</Header>
+            <Description>Drawer description</Description>
+          </Content>
+        </Drawer.Portal>
+      </Drawer.Root>
+      <Drawer.Root>
+        <Trigger data-testid="fade-from">Open fade from 300px</Trigger>
+        <Drawer.Portal>
+          <Overlay fadeFrom={300} />
+          <Content>
+            <Header>Drawer header</Header>
+            <Description>Drawer description</Description>
+          </Content>
+        </Drawer.Portal>
+      </Drawer.Root>
+      <Drawer.Root modal={false}>
+        <Trigger data-testid="custom-primitive">Open custom primitive</Trigger>
+        <Drawer.Portal>
+          <Overlay radixPrimitive={false} />
+          <Content>
+            <Header>Drawer header</Header>
+            <Description>Drawer description</Description>
+          </Content>
+        </Drawer.Portal>
+      </Drawer.Root>
+      <div className="fixed bottom-0 left-0 right-0 h-[300px] bg-red-500/15 p-4 font-medium">
+        300px
+      </div>
+    </>
+  )
+}
+
+export default Page
---
+++ apps/test/src/app/scaled-background/page.tsx
@@ -0,0 +1,47 @@
+'use client'
+
+import { Drawer } from '@vladyoslav/drawer'
+
+import { Content, Description, Header, Overlay, Trigger } from '@/shared/ui'
+
+const Page = () => {
+  return (
+    <>
+      <Drawer.Root shouldScaleBackground>
+        <Trigger>Open drawer</Trigger>
+        <Drawer.Portal>
+          <Overlay />
+          <Content>
+            <Header>Drawer header</Header>
+            <Description>Drawer description</Description>
+          </Content>
+        </Drawer.Portal>
+      </Drawer.Root>
+      <Drawer.Root shouldScaleBackground scaleFrom={200}>
+        <Trigger data-testid="scale-from">Open scale from 200px</Trigger>
+        <Drawer.Portal>
+          <Overlay />
+          <Content>
+            <Header>Drawer header</Header>
+            <Description>Drawer description</Description>
+          </Content>
+        </Drawer.Portal>
+      </Drawer.Root>
+      <Drawer.Root>
+        <Trigger data-testid="without-scaling">Open without scaling</Trigger>
+        <Drawer.Portal>
+          <Overlay />
+          <Content>
+            <Header>Drawer header</Header>
+            <Description>Drawer description</Description>
+          </Content>
+        </Drawer.Portal>
+      </Drawer.Root>
+      <div className="fixed bottom-0 left-0 right-0 h-[200px] bg-red-500/15 p-4 font-medium">
+        200px
+      </div>
+    </>
+  )
+}
+
+export default Page
