644512c359bf7aeb847df7eff1e4f51158ad4033
VladYoSlav <vaifaer@gmail.com>
2024-03-07 12:56:23 +0300

    feat(test): velocity tests

---
+++ apps/test/src/app/velocity/page.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import { Drawer } from '@vladyoslav/drawer'
+
+import { Content, Description, Header, Overlay, Trigger } from '@/shared/ui'
+
+const Page = () => {
+  return (
+    <>
+      <Drawer.Root>
+        <Trigger>Open drawer</Trigger>
+        <Drawer.Portal>
+          <Overlay />
+          <Content>
+            <Header>Drawer header</Header>
+            <Description>Drawer description</Description>
+          </Content>
+          <Drawer.SnapAreas />
+        </Drawer.Portal>
+      </Drawer.Root>
+      <Drawer.Root velocityMultiplier={0.15}>
+        <Trigger data-testid="low-velocity-multiplier">
+          Open with low velocity multiplier
+        </Trigger>
+        <Drawer.Portal>
+          <Overlay />
+          <Content>
+            <Header>Drawer header</Header>
+            <Description>Drawer description</Description>
+          </Content>
+          <Drawer.SnapAreas />
+        </Drawer.Portal>
+      </Drawer.Root>
+    </>
+  )
+}
+
+export default Page
